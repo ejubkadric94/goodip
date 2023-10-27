8374ed683f2ec7401297f2ea94e6c7147b53b223
Ejub <ejubkadric@gmail.com>
2023-10-27 23:28:32 +0200

    Fix header

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -1,22 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import MenuImage from '../images/menu.svg';
 import CloseImage from '../images/close.png'
 import CalendlyButton from '../components/CalendlyButton';
+import { createPortal } from 'react-dom';
 
 const MobileHeader = () => {
   const [expanded, setExpanded] = useState(false);
+  const menuRefLink = useRef(null);
 
-  let menu;
+  useEffect(() => {
+    if (expanded) {
+      menuRefLink.current.focus();
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [expanded]);
+
+  let menuPortal;
   if (expanded) {
-    menu = (
-      <div className='absolute top-[80px] left-0 bg-white z-1 w-full h-[calc(100%-80px)] flex flex-col pl-[16px] pr-[16px]'>    
-        <a onClick={() => setExpanded(false)} class="h-[48px] text-16 text-body-dark flex justify-around items-center" href="/#strategy">Strategy</a>
+    const menu = (
+      <div className='absolute top-[80px] left-0 bg-white w-full h-[calc(100%-80px)] flex flex-col pl-[16px] pr-[16px]'>    
+        <a ref={menuRefLink} onClick={() => setExpanded(false)} class="h-[48px] text-16 text-body-dark flex justify-around items-center" href="/#strategy">Strategy</a>
         <a onClick={() => setExpanded(false)} class="h-[48px] text-16 text-body-dark flex justify-around items-center" href="/#methods">Methods</a>
         <a onClick={() => setExpanded(false)} class="h-[48px] text-16 text-body-dark flex justify-around items-center" href="/#team">Team</a>
         <a onClick={() => setExpanded(false)} class="h-[48px] text-16 text-body-dark flex justify-around items-center" href="/#startup-stories">Startup Stories</a>
         <CalendlyButton className="w-full justify-around" client:only="react" />
       </div>
     );
+    menuPortal = createPortal(menu, document.body);
   }
 
   return (
@@ -24,7 +40,7 @@ const MobileHeader = () => {
       <button className='md:hidden' aria-expanded={expanded} onClick={() => setExpanded(!expanded)} title='Menu'>
         <img src={expanded ? CloseImage.src : MenuImage.src} alt="Menu" />
       </button>
-      {menu}
+      {menuPortal}
     </>
   );
 };
