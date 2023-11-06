73dda6afc9c9c95b93a2c25a83a5bf9a421af36f
Ejub <ejubkadric@gmail.com>
2023-11-06 16:06:30 +0100

    Send unescaped input in ContactUs

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -1,69 +1,59 @@
-export default async (req, context) => {
-  return new Response("Hello, world!" + req.body.fullName);
+const nodemailer = require('nodemailer');
+const moment = require('moment-timezone');
+
+exports.handler = async (req, res) => {
+  if (req.httpMethod !== "POST") {
+    return { statusCode: 400, body: "Accepting only POST request" };
+  }
+
+  const { fullName, email, company, question} = JSON.parse(req.body)
+
+  const germanyTime = moment().tz('Europe/Berlin').format('YYYY-MM-DD HH:mm:ss');
+
+  const gmailUser = process.env.GOOGLE_MAIL_USER;
+  const gmailSecretKey = process.env.GOOGLE_MAIL_SECRET_KEY;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: gmailUser,
+      pass: gmailSecretKey
+    }
+  });
+  
+  const emailHtml = `
+    <h1>New Inquiry</h1>
+    <p><strong>Time:</strong> ${germanyTime}</p>
+    <p><strong>Client Name:</strong> ${fullName}</p>
+    <p><strong>Client Email Address:</strong> ${email}</p>
+    <p><strong>Client Company:</strong> ${company}</p>
+    <p><strong>Client's Question:</strong> ${question}</p>
+  `;
+
+  try {
+    await transporter.sendMail({
+      from: `"${fullName}" <${email}>`,
+      to: gmailUser,
+      subject: `Goodip Inquiry from - ${fullName}`,
+      html: emailHtml,
+    }, function(error, info){
+      if(error){
+          console.log("there was an error :-(, and it was this: " + error.message);
+      }else{
+          console.log("Message sent: " + info.response);
+      }
+    });
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: "Email sent",
+  };
 };
-// import nodemailer from 'nodemailer';
-// import moment from 'moment-timezone';
-
-// export default async (event, context) => {
-//   const { fullName, email, company, question } = JSON.parse(event.body);
-
-//   const germanyTime = moment().tz('Europe/Berlin').format('YYYY-MM-DD HH:mm:ss');
-
-
-//   // const gmailUser = process.env.GOOGLE_MAIL_USER;
-//   // const gmailSecretKey = process.env.GOOGLE_MAIL_SECRET_KEY;
-//   const gmailUser = Netlify.env.get("GOOGLE_MAIL_USER");
-//   const gmailSecretKey = Netlify.env.get("GOOGLE_MAIL_SECRET_KEY");
-
-//   try {
-//     const transporter = nodemailer.createTransport({
-//       host: "smtp.gmail.com",
-//       port: 465,
-//       secure: true,
-//       auth: {
-//         user: gmailUser,
-//         pass: gmailSecretKey
-//       }
-//     });
-    
-//     const emailHtml = `
-//       <h1>New Inquiry</h1>
-//       <p><strong>Time:</strong> ${germanyTime}</p>
-//       <p><strong>Client Name:</strong> ${fullName}</p>
-//       <p><strong>Client Email Address:</strong> ${email}</p>
-//       <p><strong>Client Company:</strong> ${company}</p>
-//       <p><strong>Client's Question:</strong> ${question}</p>
-//     `;
-
-//     const info = await transporter.sendMail({
-//       from: `"${clientName}" <${clientEmail}>`,
-//       to: gmailUser,
-//       subject: `Goodip Inquiry from - ${clientName}`,
-//       html: emailHtml,
-//       // text: 'eeee'
-//     });
-//   } catch (error) {
-//     // console.log(JSON.stringify(error));
-//     // return {
-//     //   statusCode: 400,
-//     //   headers: {
-//     //     "Content-Type": "application/json",
-//     //   },
-//     //   body: JSON.stringify({
-//     //     error,
-//     //   }),
-//     // };
-//     return new Response('Error', { status: 400, statusText: JSON.stringify(error) });
-//   }
-
-//   return new Response("Success");
-//   // return {
-//   //   statusCode: 200,
-//   //   headers: {
-//   //     "Content-Type": "application/json",
-//   //   },
-//   //   body: JSON.stringify({
-//   //     msg: "Message sent successfully",
-//   //   }),
-//   // };
-// };
--- src/components/ContactUsForm.jsx
+++ src/components/ContactUsForm.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form"
 
 
 const ContactUsForm = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const {
     register,
     handleSubmit,
@@ -17,71 +19,86 @@ const ContactUsForm = () => {
 
   const onSubmit = async (data) => {
     try {
-      console.log('sending...');
       const request = await fetch(import.meta.env.PUBLIC_DOMAIN + '/.netlify/functions/contact', {
         method: 'POST',
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify({
+          fullName: data.fullName,
+          email: data.email,
+          company: data.company,
+          question: data.question,
+        }),
       });
-      console.log('sent!');
+      setIsSubmitted(true);
     } catch (error) {
       console.log('error: ', error);
     }
   };
 
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col justify-around w-full mt-[40px] md:mt-[80px]"
-    >
-      <div className="w-full md:w-[540px] mx-auto">
-        <label className="text-[18px] font-semibold text-blue-2 flex flex-col mb-[4px]">Full Name</label>
-        <input
-          {...register("fullName", { required: true })}
-          placeholder="Full Name"
-          className="w-full pt-[16px] pb-[16px] pr-[24px] pl-[24px] border border-1 border-border-primary rounded-lg placeholder:text-body-dark placeholder:leading-[21px] placeholder:text-[14px] placeholder:text-opacity-50"
-        />
-        {errors.fullName && <div className="text-[red]">This field is required</div>}
+  let content;
+  if (isSubmitted) {
+    content = (
+      <div className="leading-[48px] text-body-dark text-[20px] md:text-[24px] font-extrabold md:font-bold text-center mt-[40px]">
+        Your inquiry was sent to us. We will reach out to you as soon as possible!
       </div>
-
-      <div className="w-full md:w-[540px] mx-auto">
-        <label className="mt-[24px] text-[18px] font-semibold text-blue-2 flex flex-col mb-[4px]">Email</label>
-        <input
-          {...register("email", { required: true, pattern: { value: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, message: "Invalid email address" }})} 
-          placeholder="Email"
-          className="w-full pt-[16px] pb-[16px] pr-[24px] pl-[24px] border border-1 border-border-primary rounded-lg placeholder:text-body-dark placeholder:leading-[21px] placeholder:text-[14px] placeholder:text-opacity-50"
+    );
+  } else {
+    content = (
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col justify-around w-full mt-[40px] md:mt-[80px]"
+      >
+        <div className="w-full md:w-[540px] mx-auto">
+          <label className="text-[18px] font-semibold text-blue-2 flex flex-col mb-[4px]">Full Name</label>
+          <input
+            {...register("fullName", { required: true })}
+            placeholder="Full Name"
+            className="w-full pt-[16px] pb-[16px] pr-[24px] pl-[24px] border border-1 border-border-primary rounded-lg placeholder:text-body-dark placeholder:leading-[21px] placeholder:text-[14px] placeholder:text-opacity-50"
+          />
+          {errors.fullName && <div className="text-[red]">This field is required</div>}
+        </div>
+  
+        <div className="w-full md:w-[540px] mx-auto">
+          <label className="mt-[24px] text-[18px] font-semibold text-blue-2 flex flex-col mb-[4px]">Email</label>
+          <input
+            {...register("email", { required: true, pattern: { value: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, message: "Invalid email address" }})} 
+            placeholder="Email"
+            className="w-full pt-[16px] pb-[16px] pr-[24px] pl-[24px] border border-1 border-border-primary rounded-lg placeholder:text-body-dark placeholder:leading-[21px] placeholder:text-[14px] placeholder:text-opacity-50"
+          />
+          {errors.email && <div className="text-[red]">Invalid email</div>}
+        </div>
+  
+        <div className="w-full md:w-[540px] mx-auto">
+          <label className="mt-[24px] text-[18px] font-semibold text-blue-2 flex flex-col mb-[4px]">Company</label>
+          <input
+            {...register("company")}
+            placeholder="Company"
+            className="w-full pt-[16px] pb-[16px] pr-[24px] pl-[24px] border border-1 border-border-primary rounded-lg placeholder:text-body-dark placeholder:leading-[21px] placeholder:text-[14px] placeholder:text-opacity-50"
+          />
+        </div>
+  
+        <div className="w-full md:w-[540px] mx-auto">
+          <label className="mt-[24px] text-[18px] font-semibold text-blue-2 flex flex-col mb-[4px]">What can we help you with?</label>
+          <textarea 
+            {...register("question", { required: true })}
+            placeholder="What can we help you with?"
+            className="resize-none h-[175px] w-full pt-[16px] pb-[16px] pr-[24px] pl-[24px] border border-1 border-border-primary rounded-lg placeholder:text-body-dark placeholder:leading-[21px] placeholder:text-[14px] placeholder:text-opacity-50"
+          />
+          {errors.question && <div className="text-[red]">This field is required</div>}
+        </div>
+  
+        <input 
+          type="submit"
+          value="Send a message"
+          className="cursor-pointer md:w-[540px] mx-auto mt-[24px] text-body-dark rounded-lg bg-action font-bold text-18 h-[48px] pr-[24px] pl-[24px] flex items-center text-[18px] w-full justify-around placeholder:text-body-dark"
         />
-        {errors.email && <div className="text-[red]">Invalid email</div>}
-      </div>
+      </form>
+    );
+  }
 
-      <div className="w-full md:w-[540px] mx-auto">
-        <label className="mt-[24px] text-[18px] font-semibold text-blue-2 flex flex-col mb-[4px]">Company</label>
-        <input
-          {...register("company")}
-          placeholder="Company"
-          className="w-full pt-[16px] pb-[16px] pr-[24px] pl-[24px] border border-1 border-border-primary rounded-lg placeholder:text-body-dark placeholder:leading-[21px] placeholder:text-[14px] placeholder:text-opacity-50"
-        />
-      </div>
-
-      <div className="w-full md:w-[540px] mx-auto">
-        <label className="mt-[24px] text-[18px] font-semibold text-blue-2 flex flex-col mb-[4px]">What can we help you with?</label>
-        <textarea 
-          {...register("question", { required: true })}
-          placeholder="What can we help you with?"
-          className="resize-none h-[175px] w-full pt-[16px] pb-[16px] pr-[24px] pl-[24px] border border-1 border-border-primary rounded-lg placeholder:text-body-dark placeholder:leading-[21px] placeholder:text-[14px] placeholder:text-opacity-50"
-        />
-        {errors.question && <div className="text-[red]">This field is required</div>}
-      </div>
-
-      <input 
-        type="submit"
-        value="Send a message"
-        className="cursor-pointer md:w-[540px] mx-auto mt-[24px] text-body-dark rounded-lg bg-action font-bold text-18 h-[48px] pr-[24px] pl-[24px] flex items-center text-[18px] w-full justify-around placeholder:text-body-dark"
-      />
-    </form>
-  )
+  return <>{content}</>;
 };
 
 export default ContactUsForm;
